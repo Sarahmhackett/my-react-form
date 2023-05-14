353aef50a89909378e83c4633d12804e7f6e5f1e
Sarahmhackett <sarahmhackett@hotmail.com>
2023-05-14 07:23:07 +0100

    Starting my react project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> testing.. 1...2...3... starting my react project</p>
       </header>
     </div>
   );
